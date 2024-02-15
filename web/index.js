8f0c05d66fe396b611fe27c73a92a54d7fb7d121
benalis <sara.benali@hva.nl>
2024-02-15 15:44:10 +0100

    adding some css and validation

---
+++ web/index.js
@@ -0,0 +1,25 @@
+let dateField = document.getElementById("date-input");
+let timeField = document.getElementById("time-input");
+let nameField = document.getElementById("app_name");
+let button = document.getElementById("button");
+let errorMsg = document.querySelector(".error");
+
+
+button.onclick = function (event) {
+    event.preventDefault();
+
+    let dateInput = dateField.value;
+    let timeInput = timeField.value;
+    let nameInput = nameField.value;
+
+    if (dateInput === '') {
+        errorMsg.innerText = "Date cannot be empty!";
+    } else if (timeInput === '') {
+        errorMsg.innerText = "Time cannot be empty!";
+    } else if (nameInput === '') {
+        errorMsg.innerText = "Name cannot be empty!";
+    } else {
+        errorMsg.innerHTML = "";
+    }
+}
+
