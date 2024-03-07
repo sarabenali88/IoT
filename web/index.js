39d0397d7e136ce41d327d4a2f95cd7c7cdbe948
benalis <sara.benali@hva.nl>
2024-03-07 13:25:37 +0100

    updated my html/css/js files

--- web/index.js
+++ web/index.js
@@ -1,57 +1,48 @@
-let dateField = document.getElementById("date-input");
-let timeField = document.getElementById("time-input");
-let nameField = document.getElementById("app_name");
-let button = document.getElementById("button");
-let errorMsg = document.querySelector(".error");
+document.addEventListener("DOMContentLoaded", function() {
+    let dateField = document.getElementById("date-time-input");
+    let nameField = document.getElementById("app_name");
+    let button = document.getElementById("button");
+    let errorMsg = document.querySelector(".error");
 
-
-button.onclick = function (event) {
-    event.preventDefault();
-    checkFields();
-}
-
-function checkFields(){
-    let dateInput = dateField.value;
-    let timeInput = timeField.value;
-    let nameInput = nameField.value;
-
-    if (dateInput === '') {
-        errorMsg.innerText = "Date cannot be empty!";
-    } else if (timeInput === '') {
-        errorMsg.innerText = "Time cannot be empty!";
-    } else if (nameInput === '') {
-        errorMsg.innerText = "Name cannot be empty!";
-    } else {
-        let appointment = {
-            "date_appointment": dateInput,
-            "time_appointment": timeInput,
-            "name": nameInput
-        };
-
-        fetch("script.php", {
-            "method" : "POST",
-            "headers" : {
-                "Content-Type" : "application/json; charset=utf-8"
-            },
-            "body" : JSON.stringify(appointment)
-        }).then(function (response){
-            return response.json();
-        }).then(function(data){
-            console.log(data);
-        });
-
-        errorMsg.innerHTML = "";
-        alert("Your appointment has been added!");
-        // displayAppointment(appointment);
+    button.onclick = function (event) {
+        event.preventDefault();
+        checkFields();
     }
 
-    //  function displayAppointment(){
-    //     let card = document.querySelector("card");
-    //     card.querySelector('h6').innerText = "Date:" + dateField.value;
-    // }
-}
+    function checkFields(){
+        let dateTimeInput = dateField.value;
+        let nameInput = nameField.value;
 
+        if (dateTimeInput === '') {
+            errorMsg.innerText = "Date and Time cannot be empty!";
+        } else if (nameInput === '') {
+            errorMsg.innerText = "Name cannot be empty!";
+        } else {
+            let appointment = {
+                "date_time_appointment": dateTimeInput,
+                "name": nameInput
+            };
 
+            fetch("script.php", {
+                "method" : "POST",
+                "headers" : {
+                    "Content-Type" : "application/json; charset=utf-8"
+                },
+                "body" : JSON.stringify(appointment)
+            }).then(function (response){
+                return response.json();
+            }).then(function(data){
+                console.log(data);
+            });
 
+            errorMsg.innerHTML = "";
+            alert("Your appointment has been added!");
+            // displayAppointment(appointment);
+        }
 
-
+        //  function displayAppointment(){
+        //     let card = document.querySelector("card");
+        //     card.querySelector('h6').innerText = "Date:" + dateField.value;
+        // }
+    }
+});
