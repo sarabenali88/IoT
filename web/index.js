875c8419154ab5872c8c736e68d48315f9ca2d82
benalis <sara.benali@hva.nl>
2024-03-14 13:04:21 +0100

    changed/added styling and some js code

--- web/index.js
+++ web/index.js
@@ -1,44 +1,3 @@
-fetch("get_data.php", {
-    "method": "GET",
-    "headers": {
-        "Content-Type": "application/json; charset=utf-8"
-    }
-}).then(function (response) {
-    return response.json();
-}).then(function (data) {
-    for (let appointment of data){
-        let div = document.createElement("div");
-        div.classList.add("appointment");
-        div.innerHTML = `
-            <p>${appointment.date_time_appointment} ${appointment.name}</p> <button 
-            class="deleteButton form-control" data-id="${appointment.appointment_id}">Delete</button>`;
-        div.style.borderStyle = "solid"
-        // div.style.borderColor = "#EDC9AFFF";
-        div.style.padding = "8px";
-        div.style.margin = "10px";
-        document.getElementById("card").appendChild(div);
-
-
-        div.querySelector(".deleteButton").addEventListener("click", function(event) {
-            event.preventDefault();
-            let appointmentId = this.getAttribute('data-id');
-            fetch('delete_data.php?appointment_id=' + appointmentId, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json; charset=utf-8"
-                }
-            }).then(response => {
-                if (response.ok) {
-                    div.remove();
-                } else {
-                    console.error("Failed to delete appointment");
-                }
-            })
-        });
-    }
-    console.log(data);
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     let dateField = document.getElementById("date-input");
     let timeField = document.getElementById("time-input");
@@ -69,13 +28,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 "name": nameInput
             };
 
-            fetch("insert_data.php", {
-                "method": "POST",
-                "headers": {
-                    "Content-Type": "application/json; charset=utf-8"
-                },
-                "body": JSON.stringify(appointment)
-            }).then(function (response) {
+            fetch("insert_data.php",
+                {
+                    "method": "POST",
+                    "headers": {
+                        "Content-Type": "application/json; charset=utf-8"
+                    },
+                    "body": JSON.stringify(appointment)
+                }).then(function (response) {
                 return response.json();
             }).then(function (data) {
                 console.log(data);
@@ -84,5 +44,53 @@ document.addEventListener("DOMContentLoaded", function () {
             errorMsg.innerHTML = "";
             alert("Your appointment has been added!");
         }
+    }
+});
+
+fetch("get_data.php", {
+    "method": "GET",
+    "headers": {
+        "Content-Type": "application/json; charset=utf-8"
+    }
+}).then(function (response) {
+    return response.json();
+}).then(function (data) {
+    for (let appointment of data) {
+        let div = document.createElement("div");
+        div.classList.add("appointment");
+        let appointmentDate = new Date(appointment.date_time_appointment);
+        let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+        let formattedDate = appointmentDate.toLocaleDateString('en-GB', options);
+        let time = appointment.date_time_appointment.split(' ')[1];
+        div.innerHTML = `
+        <h6>Appointment: ${appointment.name}</h6>
+        <h6>Date: ${formattedDate}</h6>
+        <h6>Time: ${time}</h6>  
+        <button class="deleteButton mt-2 btn btn-outline-secondary" data-id="${appointment.appointment_id}">Delete</button>`;
+        div.style.borderStyle = "groove"
+        div.style.width = "50%";
+        div.style.padding = "8px";
+        div.style.margin = "10px";
+        document.getElementById("card").appendChild(div);
+
+        div.querySelector(".deleteButton").addEventListener("click", function (event) {
+            event.preventDefault();
+
+            let appointmentId = this.getAttribute('data-id');
+            fetch('delete_data.php?appointment_id=' + appointmentId, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            }).then(response => {
+                if (response.ok) {
+                    div.remove();
+                } else {
+                    console.error("Failed to delete appointment");
+                }
+            })
+        });
+
+        console.log(data);
     }
 });
