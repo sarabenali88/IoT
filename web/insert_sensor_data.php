def1170ebe4a7b36bd37f21f9b177162d053c921
benalis <sara.benali@hva.nl>
2024-03-28 14:18:46 +0100

    I added a new query for the sensor and fixed a timezone in docker

--- web/insert_sensor_data.php
+++ web/insert_sensor_data.php
@@ -1,16 +1,27 @@
 <?php
-require('connection_database.php');
-$light = $_POST["light"];
 
+/**
+ * This file is used to insert the data from the LDR sensor into the database
+ *
+ * @author Sara Benali
+ * @date 28-03-2024
+ */
+require('connection_database.php');
+
+$light = $_POST["light"];
 echo json_encode(array("light" => $light));
 
+const HTTP_STATUS_CREATED = 201;
+const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
+
 if (isset($dbConnection)) {
-    $insertQuery = $dbConnection->prepare("INSERT INTO `Sensor` (`light`) VALUES (?)");
-    $insertQuery->bind_param("s", $light);
+    $insertQuery = $dbConnection->prepare("INSERT INTO `Sensor` (`light`, `date_time`) VALUES (" . $light . ", NOW())");
     if ($insertQuery->execute()) {
-        echo json_encode(array("succes" => true));
+        http_response_code(HTTP_STATUS_CREATED);
+        echo json_encode(array("succes" => true, "message" => "Sensor data inserted succesfully"));
     } else {
-        echo json_encode(array("success" => false, "error" => $dbConnection->error));
+        http_response_code(HTTP_STATUS_INTERNAL_SERVER_ERROR);
+        echo json_encode(array("success" => false, "error" => $dbConnection->error, "message" => "No connection with database"));
     }
     $dbConnection->close();
 }
